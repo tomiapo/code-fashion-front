27f9af45f161148a199fab4aec7db1284a60843c
tomiapo <tomasapochian@gmail.com>
2023-06-28 17:07:39 -0300

    add register functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
+import RegisterContextProvider from "./store/RegisterContext";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/register"
+          element={
+            <RegisterContextProvider>
+              <Register />
+            </RegisterContextProvider>
+          }
+        />
       </Routes>
     </div>
   );

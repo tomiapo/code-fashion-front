33a0a52b93078d88b1bd2e119c088c3ba5255592
Micaela <micaelauvilla9@gmail.com>
2023-06-28 16:04:34 -0300

    create fakeDataProductor

---
+++ my-app/src/utils/fakeDataProductor.js
@@ -0,0 +1,53 @@
+const fakeDataProductor = [
+  {
+    name: "Remera pocket blanca",
+    price: 1000,
+    description: "Remera super bonita para hombre",
+    image:
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/943/997/products/boy-beige1-2e3a2fe4fc6ce264d016676887628942-1024-1024.webp",
+    stock: 5,
+    category_name: "hombre",
+    brand: "persa",
+  },
+  {
+    name: "Remera pocket negra",
+    price: 1200,
+    description: "Remera super bonita para mujer",
+    image:
+      "https://piet.com.ar/wp-content/uploads/piet-remera-negra-cuadrado-blanco-01.jpg",
+    stock: 4,
+    category_name: "mujer",
+    brand: "persa",
+  },
+  {
+    name: "Pantalon drake",
+    price: 1500,
+    description: "Pantalon super bonito para hombre",
+    image:
+      "https://marcopololp.com.ar/wp-content/uploads/2021/10/pantalon-drake-verde-old-bridge-marco-polo-1.jpg",
+    stock: 4,
+    category_name: "hombre",
+    brand: "Marco Polo",
+  },
+  {
+    name: "Pantalon anchos",
+    price: 1600,
+    description: "Pantalon super bonito para mujer",
+    image:
+      "https://media.glamour.mx/photos/61961e01eb2f10275b9757e2/1:1/w_1575,h_1575,c_limit/271645.jpg",
+    stock: 4,
+    category_name: "mujer",
+    brand: "Marco Polo",
+  },
+  {
+    name: "Pantalon Alcatraz Flex Azul",
+    price: 1400,
+    description: "Pantalon super bonito para hombre",
+    image:
+      "https://alcatraz.com.ar/wp-content/uploads/2021/09/Pantalon-Ultra-azul_0000s_0006_Pantalon-Ultra-3.jpg",
+    stock: 4,
+    category_name: "hombre",
+    brand: "Alcatraz",
+  },
+];
+module.exports = fakeDataProductor;

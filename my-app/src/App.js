87c3158e55635b1f98e09c3f03725beb27207d94
lucas <federicotoro467@gmail.com>
2023-06-28 14:19:09 -0300

    add path login in App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );

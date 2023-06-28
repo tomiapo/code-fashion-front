63c4ecdc148d9ea50c198da9403b34a0050189d0
Micaela <micaelauvilla9@gmail.com>
2023-06-28 16:03:27 -0300

    route of ProductPrevie and remove react logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import ProductPreview from "./components/ProductPreview";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProductPreview />} />
       </Routes>
     </div>
   );

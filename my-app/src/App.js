b38528cc4d460cdd7e6e54736d5a63ba157974b4
tomiapo <tomasapochian@gmail.com>
2023-06-29 04:05:20 -0300

    add register functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,21 @@ import Login from "./components/Login";
 import ProductPreview from "./components/ProductPreview";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import RegisterContextProvider from "./context/RegisterContext";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/register"
+          element={
+            <RegisterContextProvider>
+              <Register />
+            </RegisterContextProvider>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<ProductPreview />} />
       </Routes>

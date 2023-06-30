8e16e71740a59c87da77bb6a0476ca9cc6f562b4
vik1nga <victoria.fonteviva@gmail.com>
2023-06-30 03:07:59 -0300

    add to cart button/single product/cart persistency

--- my-app/src/components/ProductPreview.jsx
+++ my-app/src/components/ProductPreview.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ProductContext } from "../context/ProductContext";
 import { CartContext } from "../context/CartContext";

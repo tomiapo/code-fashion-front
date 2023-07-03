ef0515062b6da3b9238ca9b218c02c73c6cf33f4
tomiapo <tomasapochian@gmail.com>
2023-07-03 15:22:18 -0300

    created notification utils

---
+++ my-app/src/utils/notification.utils.js
@@ -0,0 +1,51 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const notifyToLoginWhenAddingToCart = () => {
+  toast.info("Inicia sesión para agregar productos al carrito", {
+    position: toast.POSITION.BOTTOM_RIGHT,
+    autoClose: 3000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    toastClassName:
+      "bg-blue-500 text-white font-bold flex items-center p-4 rounded-md",
+  });
+};
+
+export const notifyProductAlreadyInCart = (productName) => {
+  toast.info(
+    <div>
+      <strong>{productName}</strong> Ya esta en el carrito!
+    </div>,
+    {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      toastClassName:
+        "bg-blue-500 text-white font-bold flex items-center p-4 rounded-md",
+    }
+  );
+};
+
+export const notifyProductAddedToCart = (productName) => {
+  toast.success(
+    <div>
+      <strong>{productName}</strong> Agregado al carrito
+    </div>,
+    {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      toastClassName:
+        "bg-green-500 text-white font-bold flex items-center p-4 rounded-md",
+    }
+  );
+};

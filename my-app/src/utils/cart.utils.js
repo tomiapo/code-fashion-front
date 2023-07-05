8c7ed0a9901b71b1de64ae71c0b5336075d89b89
tomiapo <tomasapochian@gmail.com>
2023-07-05 00:25:14 -0300

    replace then with async await

--- my-app/src/utils/cart.utils.js
+++ my-app/src/utils/cart.utils.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { getLoggedUser } from "./login.utils";
 
-export const checkoutCart = (cartItems) => {
-  const loggedUser = getLoggedUser();
-
-  axios
-    .post(
+export const checkoutCart = async (cartItems) => {
+  try {
+    const loggedUser = getLoggedUser();
+    const result = await axios.post(
       "http://localhost:8000/api/orderhistory/create",
       { cartItems, loggedUser },
       {
         withCredentials: true,
       }
-    )
-    .then((result) => result.data)
-    .then((cart) => console.log(cart));
+    );
+
+    return result.data;
+  } catch (error) {
+    return error;
+  }
 };

9a22abf4472157a6bd6f5920c0d64ffe6a2c8bf6
tomiapo <tomasapochian@gmail.com>
2023-07-05 00:17:27 -0300

    create login utils

--- my-app/src/utils/login.utils.js
+++ my-app/src/utils/login.utils.js
@@ -1,6 +1,24 @@
-export const isLoggedIn = () => {
+const getCookiesKeysAndValues = () => {
   const cookiesKeysAndValues = document.cookie.split("=");
-  if (cookiesKeysAndValues.includes("authToken")) return true;
+  return cookiesKeysAndValues;
+};
+
+const getLoggedUserPayload = () => {
+  if (isLoggedIn()) {
+    const userIndex = getCookiesKeysAndValues().indexOf("authToken") + 1;
+    const authToken = getCookiesKeysAndValues()[userIndex].split(".");
+    const userInfoEncoded = authToken[1];
+    const decodedUserInfo = JSON.parse(atob(userInfoEncoded));
+    return decodedUserInfo.payload;
+  }
+};
+export const isLoggedIn = () => {
+  if (getCookiesKeysAndValues().includes("authToken")) return true;
 
   return false;
+};
+
+export const getLoggedUser = () => {
+  const { id, email } = getLoggedUserPayload();
+  return { id, email };
 };

10528685e56c1f5b45932778de0d83f73247794f
tomiapo <tomasapochian@gmail.com>
2023-07-07 02:30:27 -0300

    reload page on click

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { Link, useNavigate } from "react-router-dom";
-
 const Navbar = () => {
   const [userMenuOpen, setUserMenuOpen] = useState(false);
 
@@ -25,7 +24,12 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 relative lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex relative items-center">
-            <div className="flex-shrink-0 relative">
+            <div
+              className="flex-shrink-0 relative"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
               <Link to="/" className="text-white text-lg font-semibold">
                 <span className="text-indigo-500">{"<Code"}</span>
                 Fashion<span className="text-indigo-500">{" />"}</span>
@@ -34,6 +38,9 @@ const Navbar = () => {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
+                  onClick={() => {
+                    window.location.reload();
+                  }}
                   to="/"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
@@ -44,18 +51,6 @@ const Navbar = () => {
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Acerca de Nosotros
-                </Link>
-                <Link
-                  to="/category/hombre"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Hombre
-                </Link>
-                <Link
-                  to="/category/mujer"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Mujer
                 </Link>
               </div>
             </div>

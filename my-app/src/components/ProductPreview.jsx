fdcd17fe110cb0d725cd15623f725016376043c3
tomiapo <tomasapochian@gmail.com>
2023-07-06 22:15:14 -0300

    add search bar

--- my-app/src/components/ProductPreview.jsx
+++ my-app/src/components/ProductPreview.jsx
@@ -18,37 +18,39 @@ const ProductPreview = () => {
   useEffect(() => {
     productPreview();
   }, [searchBarParam]);
-  console.log("param", searchBarParam);
 
   return (
-
-    <div className="grid grid-cols-3 gap-4 relative shadow-lg py-4">
-      {products.length === 0 ? (
-        <p>No items to show</p>
-      ) : (
-        products.map(({ id, name, description, price, image, brand }) => (
-          <div
-            key={name}
-            className="bg-white rounded-md relative shadow-md p-4 flex flex-col justify-between transition duration-300 ease-in-out transform hover:scale-105"
-          >
-            <Link to={`/product/${id}`}>
-              <div>
-                <img
-                  src={image}
-                  alt={name}
-                  className="object-contain h-32 w-full mb-4"
-                />
-                <h3 className="text-xl font-bold mb-2">{name}</h3>
-                <p className="text-gray-500 mb-2">{description}</p>
-                <p className="text-gray-700 font-bold">${price}</p>
-                <p className="text-gray-500">{brand}</p>
-              </div>
-            </Link>
-          </div>
-        ))
-      )}
-    </div>
-
+    <>
+      <div>
+        <SearchBar />
+      </div>
+      <div className="grid grid-cols-3 gap-4 relative shadow-lg py-4">
+        {products.length === 0 ? (
+          <p>No items to show</p>
+        ) : (
+          products.map(({ id, name, description, price, image, brand }) => (
+            <div
+              key={name}
+              className="bg-white rounded-md relative shadow-md p-4 flex flex-col justify-between transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              <Link to={`/product/${id}`}>
+                <div>
+                  <img
+                    src={image}
+                    alt={name}
+                    className="object-contain h-32 w-full mb-4"
+                  />
+                  <h3 className="text-xl font-bold mb-2">{name}</h3>
+                  <p className="text-gray-500 mb-2">{description}</p>
+                  <p className="text-gray-700 font-bold">${price}</p>
+                  <p className="text-gray-500">{brand}</p>
+                </div>
+              </Link>
+            </div>
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
